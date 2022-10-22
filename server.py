887b4566320372e5b35c8eab28f151e16f0ed0fe
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-10-22 22:28:22 +0300

    refactor

--- server.py
+++ server.py
@@ -3,25 +3,26 @@ from db import Course, Lecture
 
 app = Flask("LMS")
 
+
 @app.route('/courses')
 def courses():
     res = {
         "lectures": [],
         "courses": []
     }
-    
+
     for l in Lecture.select():
         date, time = str(l.date).split(" ")
         date = " ".join(date.split("-")[::-1])
         time = ":".join(time.split(":")[0:2])
-        
+
         res["lectures"].append({
-           "duration": l.duration,
-           "id": str(l.id),
-           "title": l.title,
-           "description": l.description,
-           "courseId": str(l.course_id),
-           "date": date + " " + time
+            "duration": l.duration,
+            "id": str(l.id),
+            "title": l.title,
+            "description": l.description,
+            "courseId": str(l.course_id),
+            "date": date + " " + time
         })
 
     for c in Course.select():
@@ -30,7 +31,7 @@ def courses():
             "coursename": c.title,
             "coursedescription": c.description
         })
-    
+
     return str(res)
 
 
